9317cd844c1a53f2d75b26c3350034c76f43a098
Kota Kanazawa <kota0818@gmail.com>
2021-01-23 12:17:03 +0900

    各単語の文字数を先頭から出現順に並べたリストを作成

---
+++ gengosyori.rb
@@ -0,0 +1,17 @@
+##「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ。
+
+# A
+pato_car = 'パトカー'.chars
+taxi = 'タクシー'.chars
+zip_chars = pato_car.zip(taxi)
+puts zip_chars.join
+
+# B
+puts %w[パトカー タクシー].map(&:chars).transpose.join
+
+## “Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.” という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+p str.delete(".").split(/\s/).map(&:size)
+
+p str.split(/[^[:alpha:]]/).reject(&:empty?).map(&:size)
