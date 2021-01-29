cb5fc34ca85c38f1b4aa6283c9fb6018ddb039c9
Kota Kanazawa <kota0818@gmail.com>
2021-01-29 21:53:33 +0900

    Add some methods

--- cherry-4.4.2.rb
+++ cherry-4.4.2.rb
@@ -6,3 +6,12 @@ p numbers.find_all(&:even?) #=> [2, 4]
 
 # 戻り値が偽の要素を返す
 p numbers.reject(&:even?) #=> [1, 3, 5]
+
+# find
+p numbers.find(&:odd?) #=> 1
+
+# findのエイリアスメソッド
+p numbers.find(&:odd?) #=> 1
+
+# inject
+p numbers.inject(0) { |result, n| result + n } #=> 15
