ee2108a1a57b9f1164412b5a5b9b7554c8ba946d
Kota Kanazawa <kota0818@gmail.com>
2021-02-01 19:40:41 +0900

    Try dup method

---
+++ dup.rb
@@ -0,0 +1,20 @@
+ary = ['protein', 'fat', 'carb']
+copy_ary = ary.dup
+p copy_ary #=> ["protein", "fat", "carb"]
+
+# object_idはそれぞれ違う
+p ary.object_id #=> 60
+p copy_ary.object_id #=> 80
+
+# equalではない
+p ary.equal?(copy_ary) #=> false
+
+# 元の配列に変更はない
+copy_ary << 'fiber'
+p copy_ary #=> ["protein", "fat", "carb", "fiber"]
+p ary #=> ["protein", "fat", "carb"]
+
+# 破壊的変更をするとどうなるか(配列の場合)
+copy_ary.delete_if { |elm| elm == 'protein' }
+p copy_ary #=> ["fat", "carb"]
+p ary #=> ["protein", "fat", "carb"]
