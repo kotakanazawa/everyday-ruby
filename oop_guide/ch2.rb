2d740d96a4e01feda6d01706f6e8f41fb3551412
Kota Kanazawa <kota0818@gmail.com>
2021-03-04 09:45:29 +0900

    インスタンス変数はクラス内で直接参照しない

--- oop_guide/ch2.rb
+++ oop_guide/ch2.rb
@@ -18,3 +18,18 @@ class Gear
 end
 
 puts Gear.new(52, 11, 26, 1.5).ratio
+
+# インスタンス変数は隠蔽して、直接参照しない
+
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+  end
+
+  def ratio
+    @chainring / @cog.to_f #ダメ
+  end
+end
