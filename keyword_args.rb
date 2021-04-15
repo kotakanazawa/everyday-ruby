3a11a20e360f0c4ea835022e887859d5a5b35854
Kota Kanazawa <kota0818@gmail.com>
2021-04-15 11:34:50 +0900

    キーワード引数長沙

---
+++ keyword_args.rb
@@ -0,0 +1,4 @@
+def foo(k: 1)
+  p k
+end
+foo(**{k: 42})
---
+++ oop_guide/ch3/ch3.rb
@@ -0,0 +1,34 @@
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
